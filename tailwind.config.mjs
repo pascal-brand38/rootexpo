3da189f40850ff3733ae5577b63defaecd70900a
JTorkk <juho.torkkeli@icloud.com>
2025-03-22 17:15:58 +0200

    feat: add variable fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
+    fontFamily: {
+      Montserrat: ['Montserrat', 'sans-serif'],
+      JetBrainsMono: ['JetBrains Mono', 'monospace'],
+    }
   },
   plugins: [],
 };
